14998073d2ee19f5d01126bd656a7009c5b9e89f
Rex <rexblazy90@gmail.com>
2026-03-25 16:52:11 +0200

    del

--- BookingSystemPhase7/public/js/index.js
+++
@@ -1,8 +0,0 @@
-import { initAuthUI, updateHomePageUI, logout } from "./auth-ui.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initAuthUI();
-  updateHomePageUI();
-});
-
-window.logout = logout;
