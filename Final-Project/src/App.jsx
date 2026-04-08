0c5c9970a4dadad12e9948f3f80e1f077b0bd992
Rex <rexblazy90@gmail.com>
2026-04-08 16:07:10 +0300

    com

---
+++ Final-Project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from React in Docker</h1>
+      <p>This is our first containerized React app.</p>
+    </div>
+  );
+}
+
+export default App;
