a1adb15aa0e187b5b283eb9d6c7965392a64ba68
Rex <rexblazy90@gmail.com>
2026-04-24 21:34:38 +0300

    pt

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,8 +1,21 @@
-import CatalogPage from "./pages/CatalogPage";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import FormPage from "./pages/FormPage";
 
-<Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/catalog" element={<CatalogPage />} />
-  <Route path="/form" element={<FormPage />} />
-</Routes>
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/form" element={<FormPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
